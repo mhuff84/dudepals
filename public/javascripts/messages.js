401ceeeec504e43959fc89125110635579a47d04
mhuff84 <mhuff84@gmail.com>
2017-02-14 23:12:32 -0600

    more refactoring

---
+++ public/javascripts/messages.js
@@ -0,0 +1,19 @@
+$(function(){
+	$("#send").on('click', function(){
+
+		$.ajax({
+			url: $("form").attr("action"),
+			method: $("form").attr("method"),
+			async: true,
+			cache: false,
+			data: $("form").serialize(),
+			success: function(e){
+				$(".messages").prepend(e);
+			},
+			error: function(e){
+				console.log(e.responseText);
+			}
+		})		
+
+	})
+})
